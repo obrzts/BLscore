05a634b97edd59c03491bac488bc804351a76796
Anna Obraztsova <a.obraztcova@dkfz.de>
2022-01-30 16:38:04 +0100

    Add tests for input check

--- tests/testthat/test-clusterize_TCR.R
+++ tests/testthat/test-clusterize_TCR.R
@@ -1,3 +1,28 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("input check works", {
+  expect_error(clusterize_TCR(example_TCR_df, "", ".", ncores=6))
+
+  test_df = example_TCR_df %>% dplyr::rename(cdr3_beta = junction_beta)
+  expect_error(clusterize_TCR(test_df, "AB", ".", ncores=6))
+
+  test_df = example_TCR_df %>% dplyr::select(-contains("alpha"))
+  expect_error(clusterize_TCR(test_df, "AB", ".", ncores=6))
+
+  test_df = example_TCR_df %>% dplyr::select(-contains("beta"))
+  expect_error(clusterize_TCR(test_df, "B", ".", ncores=6))
+})
+
+test_that("edge case works", {
+  test_df = example_TCR_df[0,]
+  expect_error(clusterize_TCR(test_df, "", ".", ncores=6))
+
+  test_df = example_TCR_df[1,]
+  expect_error(clusterize_TCR(test_df, "", ".", ncores=6))
+})
+
+test_that("filters work", {
+  test_df = data.frame(junction_beta = c("C", "CASS*", "CASSS", "CASSS", "CASSS", "CASSS"),
+                       v_beta = c("TRBV1", "TRBV1", "TRV", "TRBV1", "TRBV1", "TRBV1"),
+                       j_beta = c("TRBJ2-4", "TRBJ2-4", "TRBJ2-4", "TRBJ", "TRBJ2-4", "TRBJ2-4"))
+  out_len = nrow(clusterize_TCR(test_df, "B", "."))
+  expect_equal(out_len, 2)
 })
